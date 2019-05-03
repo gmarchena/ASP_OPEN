162882e34e55a64e8ecea17ddb831c5aab74a29b
gmarchena <gmarchena@yahoo.com>
2019-05-03 11:35:25 -0600

    Add project files.

---
+++ src/MeowWorld2/Controllers/CatController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeowWorld2.Controllers
+{
+    public class CatController
+    {
+        [HttpGet("feline/bill")]
+        public string Meow()
+        {
+           return "Meow!";
+        }
+    }
+}
